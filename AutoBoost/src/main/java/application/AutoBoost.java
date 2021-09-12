5864291a57b0a26a655269f592d41bb72492799e
yoyo-yeung <kwyyeung@connect.ust.hk>
2021-09-12 21:43:54 +0800

    Update AutoBoost.java

--- AutoBoost/src/main/java/application/AutoBoost.java
+++ AutoBoost/src/main/java/application/AutoBoost.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ import java.util.stream.IntStream;
 
 public class AutoBoost {
     //    public static final String FILE_FORMAT = ".json";
+    private final String SCORE_FILE = "score.csv";
     private static final Logger logger = LoggerFactory.getLogger(AutoBoost.class);
     private final Results<ResultReport> testResults = new Results<ResultReport>();
     private final Results<PathCovReport> pathCovRes = new Results<PathCovReport>();
@@ -86,28 +88,28 @@ public class AutoBoost {
     public void processResults() throws IOException, org.json.simple.parser.ParseException {
         Properties properties = Properties.getInstance();
         logger.info("Processing Results in folder " + properties.getResultDir());
-        testResults.setFixedReports(new ResultReport(new File(properties.getResultDir() + "/" + Properties.FIXED_FILE_PREFIX + Properties.RESULT_FILE_SUFFIX)));
+        testResults.setFixedReports(new ResultReport(new File(properties.getResultDir() + File.separator + Properties.FIXED_FILE_PREFIX + Properties.RESULT_FILE_SUFFIX)));
         testResults.setPlausibleReports(IntStream.range(0, properties.getUnacceptedClassPaths().length).mapToObj(i -> {
             try {
-                return new ResultReport(new File(properties.getResultDir() + "/" + Properties.PATCH_FILE_PREFIX + i + Properties.RESULT_FILE_SUFFIX));
+                return new ResultReport(new File(properties.getResultDir() + File.separator + Properties.PATCH_FILE_PREFIX + i + Properties.RESULT_FILE_SUFFIX));
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
         }).filter(rep -> rep != null).collect(Collectors.toList()));
-        pathCovRes.setFixedReports(new PathCovReport(new File(properties.getResultDir() + "/" + Properties.FIXED_FILE_PREFIX + Properties.PATH_COV_FILE_SUFFIX)));
+        pathCovRes.setFixedReports(new PathCovReport(new File(properties.getResultDir() + File.separator + Properties.FIXED_FILE_PREFIX + Properties.PATH_COV_FILE_SUFFIX)));
         pathCovRes.setPlausibleReports(IntStream.range(0, properties.getUnacceptedClassPaths().length).mapToObj(i -> {
             try {
-                return new PathCovReport(new File(properties.getResultDir() + "/" + Properties.PATCH_FILE_PREFIX + i + Properties.PATH_COV_FILE_SUFFIX));
+                return new PathCovReport(new File(properties.getResultDir() + File.separator + Properties.PATCH_FILE_PREFIX + i + Properties.PATH_COV_FILE_SUFFIX));
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
         }).filter(rep -> rep != null).collect(Collectors.toList()));
-        stmtSetCovRes.setFixedReports(new StmtSetCovReport(new File(properties.getResultDir() + "/" + Properties.FIXED_FILE_PREFIX + Properties.STMT_SET_COV_FILE_SUFFIX)));
+        stmtSetCovRes.setFixedReports(new StmtSetCovReport(new File(properties.getResultDir() + File.separator + Properties.FIXED_FILE_PREFIX + Properties.STMT_SET_COV_FILE_SUFFIX)));
         stmtSetCovRes.setPlausibleReports(IntStream.range(0, properties.getUnacceptedClassPaths().length).mapToObj(i -> {
             try {
-                return new StmtSetCovReport(new File(properties.getResultDir() + "/" + Properties.PATCH_FILE_PREFIX + i + Properties.STMT_SET_COV_FILE_SUFFIX));
+                return new StmtSetCovReport(new File(properties.getResultDir() + File.separator + Properties.PATCH_FILE_PREFIX + i + Properties.STMT_SET_COV_FILE_SUFFIX));
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
