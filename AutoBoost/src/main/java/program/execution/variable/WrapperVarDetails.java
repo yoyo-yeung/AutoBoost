afd2c8baa443ab989a14e9745cf26867916f5c7a
yoyo-yeung <kwyyeung@connect.ust.hk>
2021-12-21 17:32:08 +0800

    Remove setter

--- AutoBoost/src/main/java/program/execution/variable/WrapperVarDetails.java
+++ AutoBoost/src/main/java/program/execution/variable/WrapperVarDetails.java
@@ -5,37 +5,12 @@ public class WrapperVarDetails extends VarDetail{
     Class<?> type;
     Object value;
 
-    public WrapperVarDetails() {
-    }
-
-    public WrapperVarDetails(Class<?> type) {
-        this.type = type;
-    }
-
-    public WrapperVarDetails(String typeName) throws ClassNotFoundException {
-        this(Class.forName(typeName));
-    }
-
     public WrapperVarDetails(Class<?> type, Object value) {
         this.type = type;
         this.value = value;
     }
     public WrapperVarDetails(String typeName, Object value) throws ClassNotFoundException {
         this(Class.forName(typeName), value);
-    }
-
-
-    public void setType(Class<?> type) {
-        this.type = type;
-    }
-
-    public void setType(String typeName) throws ClassNotFoundException {
-        this.setType(Class.forName(typeName));
-    }
-
-
-    public void setValue(Object value) {
-        this.value = value;
     }
 
     @Override
