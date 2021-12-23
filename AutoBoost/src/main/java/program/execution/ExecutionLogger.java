db056b215458d747fc2d95e7cab4b3fc071e1766
yoyo-yeung <kwyyeung@connect.ust.hk>
2021-12-23 14:46:39 +0800

    Add function for logging start of method execution, function to prevent infinite loop caused by toString, hashCode and equals (used in this program)

--- AutoBoost/src/main/java/program/execution/ExecutionLogger.java
+++ AutoBoost/src/main/java/program/execution/ExecutionLogger.java
@@ -12,8 +12,43 @@ public class ExecutionLogger {
     private static Stack<MethodExecution> executing = new Stack<>();
 
     public static void start(int methodId) {
+//        logger.debug(methodId);
+//        logger.debug(InstrumentResult.getSingleton().getMethodDetailsMap());
+
     }
     public static void log(int methodId, String process, String name, Object obj) {
+        ExecutionTrace trace = ExecutionTrace.getSingleton();
+        if(returnNow(methodId, process))
+            return;
+        if (obj.getClass().isArray()){
+//            logger.debug(obj);
+//            logger.debug(obj.getClass());
+//            logger.debug(obj.getClass().getSimpleName());
+//            ArrayVarDetails var = new ArrayVarDetails(obj);
+//            logger.debug(Array.get(obj,0));
+//            logger.debug(var);
+        }
+        else if(ClassUtils.isPrimitiveWrapper(obj.getClass())){}
+        else {
+            int ID = trace.getObjVarDetailsID(obj);
+            VarDetail varDetails;
+            if(ID == -1) {
+                ID = trace.getNewVarID();
+                varDetails = new ObjVarDetails(ID, obj);
+                trace.addVarDetail(varDetails);
+            }else {
+                logger.debug("find old matching");
+                logger.debug(trace.getAllVars().get(ID).getType());
+                logger.debug(obj.getClass());
+                logger.debug(trace.getAllVars().get(ID).getValue().hashCode()+"\t"+trace.getAllVars().get(ID).getValue().toString());
+                logger.debug(obj.hashCode()+"\t"+obj.toString());
+            }
+            if(LOG_ITEM.valueOf(process).equals(LOG_ITEM.CALL_THIS)) {
+                logger.debug(InstrumentResult.getSingleton().getMethodDetailsMap().get(ID));
+                executing.peek().setCalleeId(ID);
+            }
+        }
+
     }
 
     public static void log(int methodId, String process, String name, byte value) {
@@ -23,6 +58,12 @@ public class ExecutionLogger {
 
     }
     public static void log(int methodId, String process, String name, int value) {
+        try {
+//            logger.debug(new PrimitiveVarDetails(0, int.class, (Integer) value));
+        }
+        catch (Exception e) {
+            logger.error(e);
+        }
     }
     public static void log(int methodId, String process, String name, long value) {
     }
