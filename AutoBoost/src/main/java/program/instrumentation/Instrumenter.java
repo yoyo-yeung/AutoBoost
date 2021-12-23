a87fa0517d2506ddafd0d081557b409b8f246028
yoyo-yeung <kwyyeung@connect.ust.hk>
2021-12-23 18:51:46 +0800

    Add comments, logging of return value and refactoring to Instrumenter
    Refactoring to make sure same conditions for logging method entering param and exiting values

--- AutoBoost/src/main/java/program/instrumentation/Instrumenter.java
+++ AutoBoost/src/main/java/program/instrumentation/Instrumenter.java
@@ -61,37 +61,47 @@ public class Instrumenter extends BodyTransformer {
             if(!directAssgn && stmt instanceof AssignStmt && ((AssignStmt) stmt).getLeftOp().toString().indexOf("this")==0)
                 directAssgn = true;
             // log start and param values if ths method is public, NOT static initializer and NOT abstract
-            if(methodDetails.getAccess().equals(ACCESS.PUBLIC) &&!methodDetails.getType().equals(METHOD_TYPE.STATIC_INITIALIZER) && !declaringClass.isAbstract() && !paramLogged ) {
-                // log start of method
-                invExpr = Jimple.v().newStaticInvokeExpr(startLogMethod.makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.START_CALL.toString()));
-                invStmt = Jimple.v().newInvokeStmt(invExpr);
-                units.insertBefore(invStmt, stmt);
-                // log callee details if it is a member function
-                if(methodDetails.getType().equals(METHOD_TYPE.MEMBER)){
-                    invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.get(Object.class.getName()).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.CALL_THIS.toString()), StringConstant.v(body.getThisLocal().getName()), body.getThisLocal());
+            if(methodDetails.getAccess().equals(ACCESS.PUBLIC) &&!methodDetails.getType().equals(METHOD_TYPE.STATIC_INITIALIZER) && !declaringClass.isAbstract() ) {
+                if(!paramLogged ) {
+                    // log start of method
+                    invExpr = Jimple.v().newStaticInvokeExpr(startLogMethod.makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.START_CALL.toString()));
                     invStmt = Jimple.v().newInvokeStmt(invExpr);
                     units.insertBefore(invStmt, stmt);
-                }
-                // log params if there is one
-                if(body.getMethod().getParameterCount()>0)
-                    for(Local l : body.getParameterLocals()){
-                        invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.getOrDefault(l.getType().toString(), logMethodMap.get(Object.class.getName())).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.CALL_PARAM.toString()), StringConstant.v(l.getName()), l);
+                    // log callee details if it is a member function
+                    if (methodDetails.getType().equals(METHOD_TYPE.MEMBER)) {
+                        invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.get(Object.class.getName()).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.CALL_THIS.toString()), StringConstant.v(body.getThisLocal().getName()), body.getThisLocal());
                         invStmt = Jimple.v().newInvokeStmt(invExpr);
                         units.insertBefore(invStmt, stmt);
                     }
-                // set paramLogged to prevent re-logging
-                paramLogged = true;
-            }
-            if(stmt instanceof  ReturnStmt || stmt instanceof ReturnVoidStmt) {
-                if(!methodDetails.getReturnType().getClass().equals(VoidType.class)){
-//                logger.debug(((ReturnStmt)stmt).getOp());
+                    // log params if there is one
+                    if (body.getMethod().getParameterCount() > 0)
+                        for (Local l : body.getParameterLocals()) {
+                            invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.getOrDefault(l.getType().toString(), logMethodMap.get(Object.class.getName())).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.CALL_PARAM.toString()), StringConstant.v(l.getName()), l);
+                            invStmt = Jimple.v().newInvokeStmt(invExpr);
+                            units.insertBefore(invStmt, stmt);
+                        }
+                    // set paramLogged to prevent re-logging
+                    paramLogged = true;
                 }
-                if (methodDetails.getType().equals(METHOD_TYPE.CONSTRUCTOR)) {
-                    invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.get(Object.class.getName()).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.RETURN_THIS.toString()), StringConstant.v(body.getThisLocal().getName()), body.getThisLocal());
+                if(stmt instanceof  ReturnStmt || stmt instanceof ReturnVoidStmt) {
+                    if (methodDetails.getType().equals(METHOD_TYPE.CONSTRUCTOR) || methodDetails.getType().equals(METHOD_TYPE.MEMBER)) {
+                        invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.get(Object.class.getName()).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.RETURN_THIS.toString()), StringConstant.v(body.getThisLocal().getName()), body.getThisLocal());
+                        invStmt = Jimple.v().newInvokeStmt(invExpr);
+                        units.insertBefore(invStmt, stmt);
+                    }
+
+                    if(stmt instanceof ReturnStmt){
+                        Value returnOp = ((ReturnStmt)stmt).getOp();
+                        invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.getOrDefault(returnOp.getType().toString(), logMethodMap.get(Object.class.getName())).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.RETURN_ITEM.toString()), StringConstant.v("RETURN"), returnOp instanceof Local ? (Local)returnOp: returnOp);
+                    }
+                    else
+                        invExpr = Jimple.v().newStaticInvokeExpr(logMethodMap.get(String.class.getName()).makeRef(), IntConstant.v(methodId), StringConstant.v(LOG_ITEM.RETURN_VOID.toString()), StringConstant.v("RETURN"), StringConstant.v("NULL"));
                     invStmt = Jimple.v().newInvokeStmt(invExpr);
                     units.insertBefore(invStmt, stmt);
+
                 }
             }
+
         }
         methodDetails.setDirectAssignment(directAssgn);
         result.addMethod(methodDetails);
