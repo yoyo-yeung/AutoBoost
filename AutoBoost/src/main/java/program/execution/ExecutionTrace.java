cb3a77d8ae57da4364ca883652ac4d07c82dc93b
yoyo-yeung <kwyyeung@connect.ust.hk>
2022-01-27 13:55:26 +0800

    Update structure for def. execution storing. Change to custom value storing

--- AutoBoost/src/main/java/program/execution/ExecutionTrace.java
+++ AutoBoost/src/main/java/program/execution/ExecutionTrace.java
@@ -1,21 +1,21 @@
 package program.execution;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import entity.ACCESS;
 import entity.LOG_ITEM;
+import entity.METHOD_TYPE;
 import helper.Properties;
 import org.apache.commons.lang3.ClassUtils;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
+import program.analysis.MethodDetails;
 import program.execution.variable.*;
+import program.instrumentation.InstrumentResult;
 
 import java.lang.reflect.Array;
-import java.text.DecimalFormat;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -29,24 +29,15 @@ public class ExecutionTrace {
     private static final AtomicInteger varIDGenerator = new AtomicInteger(1);
     private final Map<Integer, MethodExecution> allMethodExecs;
     private final Map<Integer, VarDetail> allVars; // store all vardetail used, needed for lookups
-    private final Map<Integer, List<Integer>> varToDefMap;
+    private final Map<Integer, Integer> varToDefMap;
     private final DefaultDirectedGraph<Integer, DefaultEdge> callGraph;
     private final VarDetail nullVar;
 
-    private final Gson gson = new GsonBuilder().addSerializationExclusionStrategy(new ExclusionStrategy() {
-        @Override
-        public boolean shouldSkipField(FieldAttributes fieldAttributes) {
-            return Arrays.stream(fieldAttributes.getDeclaringClass().getDeclaredFields()).filter(field -> fieldAttributes.getName().equals(field.getName())).count() > 1;
-        }
-        @Override
-        public boolean shouldSkipClass(Class<?> aClass) {
-            return aClass.equals(java.text.DecimalFormat.class);
-        }
-    }).create();
+
     public ExecutionTrace() {
         this.allMethodExecs = new HashMap<>();
         this.allVars = new HashMap<>();
-        this.varToDefMap = new HashMap<Integer, List<Integer>>();
+        this.varToDefMap = new HashMap<Integer, Integer>();
         callGraph = new DefaultDirectedGraph<>(DefaultEdge.class);
         nullVar = new ObjVarDetails(0, Object.class, "null");
         this.allVars.put(nullVar.getID(), nullVar);
@@ -65,12 +56,12 @@ public class ExecutionTrace {
     }
 
 
-    public Map<Integer, List<Integer>> getVarToDefMap() {
+    public Map<Integer, Integer> getVarToDefMap() {
         return varToDefMap;
     }
 
-    public List<Integer> getDefExeList(Integer varID) {
-        return this.varToDefMap.getOrDefault(varID, new ArrayList<>());
+    public Integer getDefExeList(Integer varID) {
+        return this.varToDefMap.getOrDefault(varID, null);
     }
 
 
@@ -95,17 +86,67 @@ public class ExecutionTrace {
      * @return ID of the VarDetail storing the provided object
      */
     public int getVarDetailID(Class<?> type, Object objValue, LOG_ITEM process) {
+        boolean artificalEnum = false;
         if(type.isEnum()) {
             objValue = ((Enum)objValue).name();
         }
-        else if (type.equals(java.text.DecimalFormat.class) ||ClassUtils.getAllInterfaces(type).contains(DecimalFormat.class)){  // would result in serialization error if using gson
-            objValue = ToStringBuilder.reflectionToString(objValue);
-        }
         else if(!type.isArray() && !type.equals(String.class) && !ClassUtils.isPrimitiveOrWrapper(type) && !(objValue instanceof Map) && !(objValue instanceof List)&& !(objValue instanceof Set)&&!(objValue instanceof Map.Entry)) {
-            objValue = gson.toJson(objValue);
+            Object finalObjValue1 = objValue;
+            List<String> matches = Arrays.stream(type.getDeclaredFields()).filter(f -> Modifier.isPublic(f.getModifiers()) && Modifier.isStatic(f.getModifiers()) && !Modifier.isTransient(f.getModifiers()) && f.isAccessible()).filter(f -> {
+                try {
+                    Object val = f.get(finalObjValue1);
+                    return val != null && val.equals(finalObjValue1);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }).map(Field::getName).collect(Collectors.toList());
+
+            if(matches.size() > 0) {
+                artificalEnum = true;
+                objValue = matches.get(0);
+            }
+            else if(type.equals(Class.class)) {
+                artificalEnum = true;
+                type = (Class<?>) objValue;
+                objValue = "class";
+            }
+            else objValue = toStringWithAttr(objValue);
+        }
+        if(ClassUtils.isPrimitiveOrWrapper(type)) {
+            if((type.equals(double.class) || type.equals(Double.class))) {
+                if(Double.isNaN(((Double) objValue))) {
+                    artificalEnum = true;
+                    type = Double.class;
+                    objValue = "NaN";
+                }
+                else if(Double.POSITIVE_INFINITY == ((Double)objValue)) {
+                    artificalEnum = true;
+                    type = Double.class;
+                    objValue = "POSITIVE_INFINITY";
+                }
+                else if(Double.NEGATIVE_INFINITY == ((Double)objValue)) {
+                    artificalEnum = true;
+                    type = Double.class;
+                    objValue = "NEGATIVE_INFINITY";
+                }
+            }
+            if(type.equals(Integer.class) || type.equals(int.class)){
+                if(Integer.MAX_VALUE == ((Integer)objValue)) {
+                    artificalEnum = true;
+                    type = Integer.class;
+                    objValue = "MAX_VALUE";
+                }
+                else if(Integer.MIN_VALUE == ((Integer)objValue)) {
+                    artificalEnum = true;
+                    type = Integer.class;
+                    objValue = "MIN_VALUE";
+                }
+            }
         }
         int varID = objValue == null ? nullVar.getID() : findExistingVarDetailID(type, objValue, process);
         VarDetail varDetail;
+        MethodExecution latestExecution = ExecutionLogger.getLatestExecution();
         if(varID == -1) {
             if(type.isPrimitive()) {
                 try {
@@ -126,38 +167,85 @@ public class ExecutionTrace {
                 finalObjValue.forEach((key, value) -> components.put(getVarDetailID(getClassOfObj(key), key, process), getVarDetailID(getClassOfObj(value), value, process)));
                 varDetail = new MapVarDetails(getNewVarID(), type, components, objValue);
             }
+            else if(type.isEnum() || artificalEnum) {
+                varDetail = new EnumVarDetails(getNewVarID(), type, (String) objValue);
+            }
             else if (ClassUtils.isPrimitiveWrapper(type)) {
                 varDetail = new WrapperVarDetails(getNewVarID(), type, objValue);
-            }
-            else if(type.isEnum()) {
-                varDetail = new EnumVarDetails(getNewVarID(), type, (String) objValue);
             }
             else {
                 // other cases
                 varDetail = new ObjVarDetails(getNewVarID(), type, (String)objValue);
             }
             assert varDetail != null; // if null, then fail to generate test
-            if(varDetail instanceof ObjVarDetails && ((process.equals(LOG_ITEM.RETURN_THIS) && ExecutionLogger.getLatestExecution().getCalleeId()==varDetail.getID() )|| process.equals(LOG_ITEM.CALL_THIS) || process.equals(LOG_ITEM.CALL_PARAM)))
-                addVarDetailUsage(varDetail, ExecutionLogger.getLatestExecution().getID());
-            else addNewVarDetail(varDetail, ExecutionLogger.getLatestExecution().getID());
+            if(setFirstOccurrenceAsUse(varDetail, process, latestExecution))
+                addVarDetailUsage(varDetail, latestExecution.getID());
+            else addNewVarDetail(varDetail, latestExecution.getID());
 
             varID = varDetail.getID();
         }
         else {
             varDetail = this.getVarDetailByID(varID);
-            if(varDetail instanceof ObjVarDetails && (this.varToDefMap.get(varID)==null || this.varToDefMap.get(varID).size() == 0) && (process.equals(LOG_ITEM.RETURN_ITEM) || (process.equals(LOG_ITEM.RETURN_THIS) && ExecutionLogger.getLatestExecution().getCalleeId()!=varID)))
-                addNewVarDetail(varDetail, ExecutionLogger.getLatestExecution().getID());
-            else addVarDetailUsage(varDetail, ExecutionLogger.getLatestExecution().getID());
+//                if (varDetail instanceof ObjVarDetails && process.equals(LOG_ITEM.RETURN_THIS)  && result.getMethodDetailByID(latestExecution.getMethodInvokedId()).getType().equals(METHOD_TYPE.CONSTRUCTOR) && latestExecution.getParams().stream().noneMatch(p -> p == finalVarID) && (existingDef==null || (!existingDef.getType().equals(METHOD_TYPE.CONSTRUCTOR) && currentMethd.getType().equals(METHOD_TYPE.CONSTRUCTOR)) ||( !existingDef.getAccess().equals(ACCESS.PUBLIC)) && currentMethd.getAccess().equals(ACCESS.PUBLIC)))
+            if(setOccurrenceAsDef(varDetail, process, latestExecution))
+            {
+                addNewVarDetail(varDetail, latestExecution.getID());
+            }
+            else {
+                addVarDetailUsage(varDetail, latestExecution.getID());
+            }
         }
         return varID;
     }
 
+    private boolean setFirstOccurrenceAsUse(VarDetail varDetail, LOG_ITEM process, MethodExecution execution) {
+        if(!(varDetail instanceof ObjVarDetails))
+            return true;
+        MethodDetails details = InstrumentResult.getSingleton().getMethodDetailByID(execution.getMethodInvokedId());
+        if(details.getAccess().equals(ACCESS.PRIVATE) || (details.getAccess().equals(ACCESS.PROTECTED) && !details.getDeclaringClass().getPackageName().equals(Properties.getSingleton().getGeneratedPackage())))
+            return true;
+        switch(process) {
+            case CALL_THIS:
+            case CALL_PARAM:
+                return true;
+            case RETURN_THIS:
+            case RETURN_ITEM:
+                return (execution.getCalleeId() == varDetail.getID() || execution.getParams().contains(varDetail.getID()));
+            default:
+                return false;
+        }
+    }
+
+    private boolean setOccurrenceAsDef(VarDetail varDetail, LOG_ITEM process, MethodExecution execution) {
+        if(!(varDetail instanceof ObjVarDetails))
+            return false;
+        if(!process.equals(LOG_ITEM.RETURN_THIS) && !process.equals(LOG_ITEM.RETURN_ITEM)) return false;
+        MethodDetails details = InstrumentResult.getSingleton().getMethodDetailByID(execution.getMethodInvokedId());
+        MethodExecution existingDefEx = getDefExeList(varDetail.getID()) == null ? null : getMethodExecutionByID(getDefExeList(varDetail.getID()));
+        MethodDetails existingDef = existingDefEx == null ? null : InstrumentResult.getSingleton().getMethodDetailByID(existingDefEx.getMethodInvokedId());
+        if(details.getAccess().equals(ACCESS.PRIVATE) || (details.getAccess().equals(ACCESS.PROTECTED) && !details.getDeclaringClass().getPackageName().equals(Properties.getSingleton().getGeneratedPackage())))
+            return false;
+        if(execution.getCalleeId() == varDetail.getID() || execution.getParams().contains(varDetail.getID()))
+            return false;
+        if(existingDef == null) return true;
+        else if(existingDef.getId() == details.getId()) return false;
+
+        int existingNullDefCount = getNullDefCount(existingDefEx);
+        int currentNullDefCount = getNullDefCount(execution);
+        if(existingNullDefCount > currentNullDefCount) return true;
+        else if(existingNullDefCount == currentNullDefCount) return ((existingDef.getParameterCount() + (existingDef.getType().equals(METHOD_TYPE.CONSTRUCTOR) || existingDef.getType().equals(METHOD_TYPE.STATIC)? 0: 1)) > (details.getParameterCount() + (details.getType().equals(METHOD_TYPE.STATIC) || details.getType().equals(METHOD_TYPE.CONSTRUCTOR) ? 0 : 1)));
+        return false;
+    }
+
+    private int getNullDefCount(MethodExecution execution) {
+        return (int) execution.getParams().stream().map(this::getVarDetailByID).filter(p ->( (p instanceof ObjVarDetails) && getDefExeList(p.getID()) == null )).count() + ((execution.getCalleeId() == -1 || getDefExeList(execution.getCalleeId()) != null) ? 0: 1);
+    }
     private Stream<Integer> getComponentStream(Class<?> type, Object obj, LOG_ITEM process) {
         if(!ArrVarDetails.availableTypeCheck(type))
             throw new IllegalArgumentException("Provided Obj cannot be handled.");
         Stream<Integer> componentStream;
         if(type.isArray()){
-            componentStream = IntStream.range(0, Array.getLength(obj)).mapToObj(i -> getVarDetailID(obj.getClass().getComponentType(), Array.get(obj, i), process));
+            componentStream = IntStream.range(0, Array.getLength(obj)).mapToObj(i -> getVarDetailID( getClassOfObj( Array.get(obj, i)), Array.get(obj, i), process));
         }
         else
             componentStream = ((Collection) obj).stream().map(v -> getVarDetailID(getClassOfObj(v), v, process));
@@ -195,8 +283,8 @@ public class ExecutionTrace {
      * @param varID key of VarDetail
      */
     private void setUpVarMap(int varID) {
-        if(!this.varToDefMap.containsKey(varID) || this.varToDefMap.get(varID) == null)
-            this.varToDefMap.put(varID, new ArrayList<>());
+        if(!this.varToDefMap.containsKey(varID))
+            this.varToDefMap.put(varID, null);
     }
 
 
@@ -211,7 +299,7 @@ public class ExecutionTrace {
         this.setUpVarMap(detail.getID());
         // only store it as a def if it is an object (need construction)
         if(detail instanceof ObjVarDetails)
-            this.varToDefMap.get(detail.getID()).add(executionID);
+            this.varToDefMap.put(detail.getID(), executionID);
     }
 
     /**
@@ -251,12 +339,49 @@ public class ExecutionTrace {
     }
 
     public MethodExecution getMethodExecutionByID(int exeID) {
-        if(!this.allMethodExecs.containsKey(exeID))
+        Stack<MethodExecution> executing = ExecutionLogger.getExecuting();
+        if(!this.allMethodExecs.containsKey(exeID) && executing.stream().noneMatch(e -> e.getID()==exeID))
             throw new IllegalArgumentException("MethodExecution with ID " + exeID + " does not exist");
+        else if (!this.allMethodExecs.containsKey(exeID)) {
+            return executing.stream().filter(e -> e.getID()==exeID).findFirst().get();
+        }
         return this.allMethodExecs.get(exeID);
     }
 
     public VarDetail getNullVar() {
         return nullVar;
+    }
+
+    private String toStringWithAttr(Object obj) {
+        Stack<MethodExecution> executing = ExecutionLogger.getExecuting();
+        int existing = executing.size();
+        if (obj == null) return "null";
+        String val =  baseFieldsToString(obj, 4);
+//        logger.debug(val);
+        return val;
+    }
+    private String baseFieldsToString(Object obj, int depth) {
+        if(obj==null ) return "null";
+        else if(obj.getClass().isArray()) return "["+IntStream.range(0, Array.getLength(obj)).mapToObj(i -> Array.get(obj, i) == null ? "null" : baseFieldsToString(Array.get(obj, i), depth-1)).collect(Collectors.joining(",")) + "]";
+        else if (obj instanceof Map) return "{"+((Map<?,?>)obj).entrySet().stream().map(e -> "{"+baseFieldsToString(e.getKey(), depth -1)+"="+baseFieldsToString(e.getValue(), depth-1)+"}").collect(Collectors.joining(","))+ "}";
+        else if (obj instanceof Collection) return "{"+String.valueOf(((Collection) obj).stream().map(i -> baseFieldsToString(i, depth-1)).collect(Collectors.joining(",")))+ "}";
+        else if(ClassUtils.isPrimitiveOrWrapper(obj.getClass()) || obj.getClass().getName().startsWith("java")) return String.valueOf(obj);
+        else if (depth==0) return obj.getClass().getName() + "[]";
+        List<Class> classesToGetFields = new ArrayList<>();
+        classesToGetFields.add(obj.getClass());
+        classesToGetFields.addAll(ClassUtils.getAllSuperclasses(obj.getClass()));
+        classesToGetFields.removeIf(c -> c.equals(Object.class));
+        return "{"+classesToGetFields.stream().filter(c -> !c.getName().startsWith("java") && !ClassUtils.isPrimitiveOrWrapper(c))
+                .map(c -> {
+            return c.getName() + "[" + Arrays.stream(c.getDeclaredFields()).filter(f -> !(Modifier.isStatic(f.getModifiers()) && Modifier.isFinal(f.getModifiers()))).map(f -> {
+                try {
+                    f.setAccessible(true);
+                    return f.getName()+"=" + baseFieldsToString(f.get(obj), depth -1);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }).filter(Objects::nonNull).map(Object::toString).collect(Collectors.joining(",")) +"]";
+        }).collect(Collectors.joining(",")) +"}";
     }
 }
