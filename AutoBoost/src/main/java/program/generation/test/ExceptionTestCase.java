0f60d9014676fca41e5583904e9e10e9c6880f3c
Yoyo <kwyyeung@ust.hk>
2022-08-25 13:11:45 +0800

    feat: add exception checking ability to junit 3

--- AutoBoost/src/main/java/program/generation/test/ExceptionTestCase.java
+++ AutoBoost/src/main/java/program/generation/test/ExceptionTestCase.java
@@ -18,10 +18,15 @@ public class ExceptionTestCase extends TestCase{
     public String output(Set<Class<?>> fullCNameNeeded) {
         String indentation = "\t";
         StringBuilder result = new StringBuilder();
+        String contentIndentation = Properties.getSingleton().getJunitVer()==3? indentation+indentation+indentation : indentation+indentation;
         if(Properties.getSingleton().getJunitVer()!=3)
             result.append(indentation).append("@Test (expected = ").append(exceptionClass.getName()).append(".class").append(")\n");
         result.append(indentation).append("public void test").append(this.getID()).append("() throws Exception {\n");
-        result.append(outputStmts(indentation+indentation, fullCNameNeeded));
+        if(Properties.getSingleton().getJunitVer() == 3)
+            result.append(indentation).append(indentation).append("try{").append("\n");
+        result.append(outputStmts(contentIndentation, fullCNameNeeded));
+        if(Properties.getSingleton().getJunitVer() == 3)
+            result.append(indentation).append(indentation).append("fail(\"Expected exception\");\n").append(indentation).append("}catch(").append(exceptionClass.getName()).append(" ignored){\n").append(indentation).append(indentation).append("}\n");
         result.append(indentation).append("}\n");
         return result.toString();
     }
