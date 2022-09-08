9e5c58a573512a54fa28864d300db9906025576d
Yoyo <kwyyeung@ust.hk>
2022-09-08 13:03:36 +0800

    feat: add methods for getting callee and method invoke string separately

--- AutoBoost/src/main/java/program/execution/stmt/MethodInvStmt.java
+++ AutoBoost/src/main/java/program/execution/stmt/MethodInvStmt.java
@@ -2,15 +2,18 @@ package program.execution.stmt;
 
 import entity.METHOD_TYPE;
 import helper.Properties;
+import org.apache.commons.lang3.ClassUtils;
 import program.analysis.MethodDetails;
+import program.execution.ExecutionTrace;
 import program.instrumentation.InstrumentResult;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class MethodInvStmt extends Stmt{
+public class MethodInvStmt extends Stmt {
     private final String callee;
     private final int methodInvID;
     private final List<Stmt> paramStmts;
@@ -21,17 +24,27 @@ public class MethodInvStmt extends Stmt{
         this.paramStmts = paramStmts;
     }
 
+    public String getCallee() {
+        return callee;
+    }
+
+    public String getInstanceCallString(Set<Class<?>> fullCNameNeeded) {
+        MethodDetails details = InstrumentResult.getSingleton().getMethodDetailByID(methodInvID);
+        if (!details.getType().equals(METHOD_TYPE.MEMBER)) return "";
+        return details.getName().replace("$", ".") + "(" + paramStmts.stream().map(s -> s.getStmt(fullCNameNeeded)).collect(Collectors.joining(Properties.getDELIMITER())) + ")";
+    }
+
     @Override
     public String getStmt(Set<Class<?>> fullCNameNeeded) {
-        MethodDetails details  = InstrumentResult.getSingleton().getMethodDetailByID(methodInvID);
-        return (callee == null ? "" : callee) + (details.getType().equals(METHOD_TYPE.CONSTRUCTOR) ? ("new " + (fullCNameNeeded.contains(details.getdClass()) ? details.getdClass().getName().replace("$", ".") : details.getdClass().getSimpleName().replace("$", "."))): (callee == null ? "" : ".") + details.getName().replace("$", ".")) + "(" + paramStmts.stream().map(s-> s.getStmt(fullCNameNeeded)).collect(Collectors.joining(Properties.getDELIMITER())) + ")";
+        MethodDetails details = InstrumentResult.getSingleton().getMethodDetailByID(methodInvID);
+        return (callee == null ? "" : callee) + (details.getType().equals(METHOD_TYPE.CONSTRUCTOR) ? ("new " + (fullCNameNeeded.contains(details.getdClass()) ? details.getdClass().getName().replace("$", ".") : details.getdClass().getSimpleName().replace("$", "."))) : (callee == null ? "" : ".") + details.getName().replace("$", ".")) + "(" + paramStmts.stream().map(s -> s.getStmt(fullCNameNeeded)).collect(Collectors.joining(Properties.getDELIMITER())) + ")";
     }
 
     @Override
     public Set<Class<?>> getImports(String packageName) {
         Set<Class<?>> results = new HashSet<>();
         METHOD_TYPE methodType = InstrumentResult.getSingleton().getMethodDetailByID(methodInvID).getType();
-        if(methodType.equals(METHOD_TYPE.CONSTRUCTOR) || methodType.equals(METHOD_TYPE.STATIC) || callee.contains(".")) // callee contains . -> enum type
+        if (methodType.equals(METHOD_TYPE.CONSTRUCTOR) || methodType.equals(METHOD_TYPE.STATIC) || callee.contains(".")) // callee contains . -> enum type
             results.add(getTypeToImport(InstrumentResult.getSingleton().getMethodDetailByID(methodInvID).getdClass(), packageName));
         this.paramStmts.forEach(stmt -> results.addAll(stmt.getImports(packageName)));
         results.remove(null);
